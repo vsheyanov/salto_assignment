a07bc91d1ad0d22c06b180ec477c0e26954e321f
vsheyanov <victor.sheyanov@gmail.com>
2019-12-07 22:17:02 +0300

    added repositories-list.test.tsx

--- src/components/repositories-view/repositories-list.test.tsx
+++ src/components/repositories-view/repositories-list.test.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import RepositoriesList from './repositories-list';
+import { RepositorySearchItem } from "../../model/interfaces";
 
-test('renders learn react link', () => {
+test('empty repositories list', () => {
   const { getByText } = render(<RepositoriesList items={ [] }/>);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/No respositories found/);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('empty repositories list', () => {
+  const item: RepositorySearchItem = {
+    id: 12123,
+    name: 'super-repo',
+    full_name: 'victor/super-repo',
+    owner: {
+      id: 23234,
+      login: 'victor',
+    },
+  };
+  const { queryByText } = render(
+      <Router>
+        <RepositoriesList items={ [item] }/>
+      </Router>
+  );
+  const linkElement = queryByText(/No respositories found/);
+
+  expect(linkElement).not.toBeInTheDocument();
+  expect(queryByText(item.owner.login)).toBeInTheDocument();
+  expect(queryByText(item.full_name)).toBeInTheDocument();
 });
