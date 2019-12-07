5f6612f8b6b6dab712e4145afbdf917c163d6dbc
vsheyanov <victor.sheyanov@gmail.com>
2019-12-07 22:09:12 +0300

    refactoring

--- src/components/paginator/paginator.test.tsx
+++ src/components/paginator/paginator.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import Paginator from './paginator';
